5f87ac63bfff6409f5b7c22911cb35d4e2993530
gdonados <79154150+gdonados@users.noreply.github.com>
2022-02-05 20:39:07 -0500

    Joystick mapping started, need new joystick

--- source/servoTesting/servoTesting.ino
+++ source/servoTesting/servoTesting.ino
@@ -1,21 +1,69 @@
-#include <Servo.h>
+#include <Servo.h>  //Include header files
 
-int servoPin = 3;
-int val = 0;
+//Define pin assignments
+#define servoPWM  3
+#define selPin    6
+#define joystickX A0
+#define joystickY A1
 
+//Create servo object to use library
 Servo servo;
 
+uint16_t posX, posY = 0;     //Joystick position variables
+uint16_t servo0degree = 0;   //Variable for knowing when motor is in the 0 degree position
+uint16_t servo180degree = 0; //Variable for servo 180 position
+uint8_t switchPress = 0;//Switch press, either 1 or 0
+
+//Initial code setup
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(servoPin, OUTPUT);
-  servo.attach(servoPin);
-  val = map(val, 0, 1023, 0, 180); 
+  //Setup pin modes
+  pinMode(servoPWM, OUTPUT);
+  pinMode(selPin, INPUT_PULLUP);
+  pinMode(joystickX, INPUT);
+  pinMode(joystickY, INPUT);
+  
+  //Attach servo signal wire to specified pin
+  servo.attach(servoPWM);
+
+  /* Setup serial monitor for communication with computer
+   * Uses Arduino UART communication to send/receive data
+   * Just being used for debugging/testing in this case
+   * 
+   * @param Baud Rate
+   * Sets transmission rate for serial communication
+   */
+  Serial.begin(9600);
 }
 
+//Main program loop
 void loop() {
-  // put your main code here, to run repeatedly:
-  servo.write(1000);
-  delay(500);
-  servo.write(0);
-  delay(500);
+  //Read the values from all pins, 
+  //analog read for analog pins, digital read for digital pins
+  posX = analogRead(joystickX);
+  posY = analogRead(joystickY);
+  switchPress = digitalRead(selPin);
+  //Print out values for debugging
+  debugPrint(joystickX, joystickY, 0, 0);
+  //Delay as to not bog down output
+  delay(100);
+}
+
+/*
+ * Take 4 separate readings to be formatted
+ * then printed. If less than 4 values are used
+ * replace with 0
+ * 
+ * @param val1 val2 val3 val4
+ * Values to be printed to the serial terminal
+ */
+void debugPrint(int val1, int val2, int val3, int val4)
+{
+  Serial.print("Value 1: ");
+  Serial.print(val1);
+  Serial.print("   Value 2: ");
+  Serial.print(val2);
+  Serial.print("   Value 3: ");
+  Serial.print(val3);
+  Serial.print("   Value 4: ");
+  Serial.println(val4);
 }
--- source/servoTesting/servo_README.txt
+++ source/servoTesting/servo_README.txt
@@ -3,3 +3,8 @@ Code for testing servo movements.
 Expand to include multiple servos.
 
 Document any pinouts below:
+Joystick: GND-GND ; 5V-5V ; VRX-A0 ; VRY-A1 ; SEL-D6
+Servo: GND-GND ; VCC-External Supply ; Sig-D3 ; Feedback-A6
+
+Currently setting up joystick to map to servo movement, my joystick I was testing
+seems to be broken, so I need to try one of the new ones.
